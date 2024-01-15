92d9698893bcfda09414386765a79fc239c18bc6
Alain Turcotte <alain.turcotte2@gmail.com>
2024-01-14 21:14:37 -0500

    first commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!1jr7+4rfm$j%j3+tpslgl1oa8ny@apq^f!qg3=or7wn55cq0d"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
